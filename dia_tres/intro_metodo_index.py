5fbbcb78f502e6cc7f2f468f2a302dd3a9a6b0c2
Bruno Coronado <brunocoronado@Braunybrunista666.local>
2023-04-01 23:02:39 -0600

    dia tres del curso

---
+++ dia_tres/intro_metodo_index.py
@@ -0,0 +1,10 @@
+mi_texto = "Esta es una prueba"
+
+resultado = mi_texto[8]
+print(resultado)
+
+otro_resultado = mi_texto.index("n")
+otro_resultado = mi_texto.index("a", 5)
+print(otro_resultado)
+
+metodo_rindex = mi_texto.rindex("a")
---
+++ dia_tres/intro_metodos_string.py
@@ -0,0 +1,29 @@
+texto = "Este es el texto de Bruce"
+
+result = texto.upper()
+print(result)
+
+result = texto[2].upper()
+print(result)
+
+result = texto.lower()
+print(result)
+
+result = texto[0].upper()
+print(result)
+
+result = texto.split()
+print(result)
+
+result = texto.split("t")
+print(result)
+
+a = "Aprender"
+b = "Python"
+c = "es"
+d = "genial"
+e = " ".join([a,b,c,d])
+print(e)
+
+resultado = texto.replace("e", "r")
+print(resultado)
