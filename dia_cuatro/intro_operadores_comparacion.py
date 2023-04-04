94e08ba8b08c343a858b9b07ea0be941e46e0d80
Bruno Coronado <brunocoronado@Braunybrunista666.local>
2023-04-04 14:22:02 -0600

    Empezando dia 4

---
+++ dia_cuatro/intro_operadores_comparacion.py
@@ -0,0 +1,15 @@
+mi_bool = 10 == 23
+print(mi_bool)
+
+mi_color = "blanco" == "negro"
+print(mi_color)
+
+mi_color = "Blanco" == "blanco"
+print(mi_color)
+
+mi_numero = 100.0 == 100
+print(mi_numero)
+
+mi_numero = 100 != 100
+print(mi_numero)
+
---
+++ dia_cuatro/intro_operadores_logicos.py
@@ -0,0 +1,15 @@
+mi_bool = (4 < 5) and (5 > 6)
+print(mi_bool)
+
+mi_bool = (4 > 5) or (7 < 10)
+print(mi_bool)
+
+frase = "Hola soy una frase"
+mi_bool = ("una" in frase) or ("python" in frase)
+print(mi_bool)
+
+mi_bool = not "a" == "a"
+print(mi_bool)
+
+mi_bool = not "a" != "a"
+print(mi_bool)
