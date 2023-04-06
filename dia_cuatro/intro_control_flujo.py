5f3c01d31955a1fab3fd5e26d77cfee557b0d9e2
Bruno Coronado <brunocoronado@Braunybrunista666.local>
2023-04-05 23:50:39 -0600

    intro dia 4

---
+++ dia_cuatro/intro_control_flujo.py
@@ -0,0 +1,28 @@
+mi_vaar = 10 < 9
+
+if mi_vaar:
+    print("Ahuevo bandamax")
+else:
+    print("Nel prrines")
+    
+mascota = "perro"
+if mascota == "gato":
+    print("tienes un gato")
+elif mascota == "perro":
+    print("tienes un perro")
+elif mascota == "conejo":
+    print("tienes un conejo")
+else:
+    print("tienes pura riata wey jajaja")
+
+edad = 15
+calif = 9
+
+if edad < 18:
+    print("Eres menor de edad")
+    if calif < 7:
+        print("Nambre mi chavo ustede reprobo bien feo")
+    else:
+        print("Aaaahh prrin tas cabron")
+else:
+    print("Eres adulto")
---
+++ dia_cuatro/intro_loops.py
@@ -0,0 +1,9 @@
+mi_lista = ["Bruce", "Maria", "John", "Franco"]
+
+for name in mi_lista:
+    print(f"Hola {name}!")
+    
+lista = ["a", "b", "c"]
+
+for letra in lista:
+    print(letra)
