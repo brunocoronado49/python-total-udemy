f4cdad19aafe62a3ebe1102a1b2091dc92375859
Bruno Coronado <brunocoronado@Braunybrunista666.local>
2023-04-11 14:44:18 -0600

    Interaccion de funciones

---
+++ dia_cinco/ejemplo_funcion.py
@@ -0,0 +1,24 @@
+precios_cafe = [("capuccino", 1.5), ("expresso", 1.20), ("Moka", 1.9)]
+
+for cafe, precio in precios_cafe:
+    print(cafe, precio)
+
+
+def cafe_caro(lista_precios):
+    precio_mayor = 0
+    cafe_mas_caro = ""
+
+    for cafe, precio in lista_precios:
+        if precio > precio_mayor:
+            precio_mayor = precio
+            cafe_mas_caro = cafe
+        else:
+            pass
+
+    return (cafe_mas_caro, precio_mayor)
+
+
+print(cafe_caro(precios_cafe))
+
+cafe, precio = cafe_caro(precios_cafe)
+print(f"El cafe mas caro es el {cafe} con un precio de {precio}")
---
+++ dia_cinco/interaccion_funciones.py
@@ -0,0 +1,35 @@
+from random import shuffle
+
+# Lista inicial
+palitos = ["-", "--", "---", "----"]
+
+
+# Mezclar palitos
+def mezclar(lista):
+    shuffle(lista)
+    return lista
+
+
+# Pedirle intento
+def probar_suerte():
+    intento = ""
+    while intento not in ['1', "2", "3", "4"]:
+        intento = input("Elige un numero del 1 al 4: ")
+
+    return int(intento)
+
+
+# Comprobar intento
+def checar_intento(lista, intento):
+    if lista[intento - 1] == '-':
+        print("A lavar los platos")
+    else:
+        print("Te has salvado")
+
+    print(f"Te ha tocado {lista[intento - 1]}")
+
+
+palitos_mezclados = mezclar(palitos)
+seleccion = probar_suerte()
+revizar = checar_intento(palitos_mezclados, seleccion)
+print(revizar)
