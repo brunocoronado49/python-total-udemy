259cbf715d1f8224521ac31509294439ecf9d7ab
Bruno Coronado <brunocoronado@Braunybrunista666.local>
2023-04-13 00:46:21 -0600

    preparando para formatear

--- dia_cinco/intro_keyword_args.py
+++ dia_cinco/intro_keyword_args.py
@@ -14,3 +14,20 @@ def suma_nueva(**kwargs):
         
         
 suma_nueva(x = 3, y = 5, z = 2)
+
+def prueba_varios(a, b, *args, **kwargs):
+    print(f"numero uno {a}")
+    print(f"numero dos {b}")
+    
+    for arg in args:
+        print(f"esto es un arg: {arg}")
+        
+    for clave, valor in kwargs.items():
+        print(f"{clave} = {valor}")
+        
+
+prueba_varios(10, 20, 45, 60, 74, x = "uno", y = "dos")
+
+args = [ 45, 60, 74]
+kwargs = {"x": "uno", "y": "dos"}
+prueba_varios(10, 20, *args, **kwargs)
