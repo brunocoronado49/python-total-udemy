aef0b9adcf161188d7f92efb56c914441967f623
Francisco Rangel <francorangelcoronado@gmail.com>
2024-02-21 15:12:19 -0600

    preactica dia 5

--- 05-dia/intro_funciones_dinamicas.py
+++ 05-dia/intro_funciones_dinamicas.py
@@ -2,8 +2,7 @@ def checar_cifras(numero):
     return numero in range(100, 1000)
 
 
-resultado = checar_cifras(657)
-print(resultado)
+print(checar_cifras(657))
 
 
 def checar_lista(lista):
@@ -18,3 +17,25 @@ def checar_lista(lista):
 
 resultado_lista = checar_lista([103, 2170, 30, 100])
 print(resultado_lista)
+
+
+def positive_numbers(list_nums):
+    for num in list_nums:
+        if num < 0:
+            return False
+    
+    return True
+
+print(positive_numbers([1, 2, 3, 4, 5, -1, 2, -5]))
+
+
+def sum_numbers(list_nums):
+    sum = 0
+    for num in list_nums:
+        if num > 0 and num < 1000:
+            sum += num
+    
+    return sum
+
+
+print(sum_numbers([2, 3, 6, 500, 877, 321, 10]))
---
+++ 05-dia/test.py
@@ -0,0 +1,4 @@
+from random import choice
+
+print(choice([['TS/JS', 'React', 'NextJS', 'Vue', 'React Native', 'Frontend'],
+              ['Python', 'Backend', 'Django', 'Flask', 'FASTAPI']]))
