9a941c6b486123e55305cfb2fbf19eda185b3189
Francisco Rangel <francorangelcoronado@gmail.com>
2024-03-25 11:51:16 -0600

    creando y modificando, ejercicios listos

--- 06-dia/write_create_files.py
+++ 06-dia/write_create_files.py
@@ -13,3 +13,24 @@ for p in my_list:
     my_new_file.writelines(p + '\n')
 
 my_new_file.close()
+
+# Ejercicios
+mi_archivo = open('mi_archivo.txt', 'w')
+mi_archivo.write('Nuevo texto')
+mi_archivo.close()
+mi_archivo = open('mi_archivo.txt')
+print(mi_archivo.read())
+
+mi_archivo = open('mi_archivo.txt', 'a')
+mi_archivo.write("Nuevo inicio de sesión")
+mi_archivo.close()
+mi_archivo = open('mi_archivo.txt', 'r')
+print(mi_archivo.read())
+
+registro_ultima_sesion = ["Federico", "20/12/2021", "08:17:32 hs", "Sin errores de carga"]
+registro = open('registro.txt', 'a')
+for l in registro_ultima_sesion:
+    registro.writelines(l + '\t')
+registro.close()
+registro = open('registro.txt', 'r')
+print(registro.read())
