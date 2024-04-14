46cae925b18c42c212b8f849893fbb31cb7aa593
Francisco Rangel <francorangelcoronado@gmail.com>
2024-04-14 00:06:56 -0600

    terminando y probando el programa

--- 06-dia/proyecto_dia_seis.py
+++ 06-dia/proyecto_dia_seis.py
@@ -34,7 +34,7 @@ def inicio():
               6) Salir del programa
               ''')
         eleccion_menu = input()
-    return eleccion_menu
+    return int(eleccion_menu)
     
 
 def mostrar_categorias(ruta):
@@ -79,45 +79,100 @@ def elegir_receta(lista):
     while not eleccion_correcta.isnumeric() or int(eleccion_correcta) not in range(1, len(lista) - 1):
         eleccion_correcta = input('\nElige una receta: ')
     return lista[int(eleccion_correcta) - 1]
+
+
+def leer_receta(receta):
+    print(Path.read_text(receta))
+    
+
+def crear_receta(ruta):
+    existe = False
+    
+    while not existe:
+        print('Escribe el nombre de tu receta: ')
+        nombre_receta = input() + '.txt'
+        print('Escribe la receta: ')
+        contenido_receta = input()
+        ruta_nueva = Path(ruta, nombre_receta)
         
+        if not os.path.exists(ruta_nueva):
+            Path.write_text(ruta_nueva, contenido_receta)
+            print(f'Tu receta {nombre_receta} se ha creado')
+            existe = True
+        else:
+            print('Esa receta ya existe')
+            
+            
+def crear_categoria(ruta):
+    existe = False
+    
+    while not existe:
+        print('Escribe el nombre de tu receta: ')
+        nombre_categoria = input() + '.txt'
+        ruta_nueva = Path(ruta, nombre_categoria)
+        
+        if not os.path.exists(ruta_nueva):
+            Path.mkdir(ruta_nueva)
+            print(f'Tu categoria {nombre_categoria} se ha creado')
+            existe = True
+        else:
+            print('Esa categoria ya existe')
+            
+            
+def eliminar_receta(receta):
+    Path(receta).unlink()
+    print(f'La receta {receta.name} se ha eliminado')
+    
 
+def eliminar_categoria(categoria):
+    Path(categoria).rmdir()
+    print(f'La categoria {categoria.name} se ha eliminado')
+    
+    
+def volver_inicio():
+    eleccion_volver = 'x'
+    
+    while eleccion_volver.lower() != 'v':
+        eleccion_volver = input('Ingresa "v" para volver al menu: ')
 
-# Mostrar menu inicio
-menu = 0
+finalizar = False
 
-match menu:
-    case 1:
-        mis_categorias = mostrar_categorias(ruta)
-        mi_categoria = elegir_categorias(mis_categorias)
-        mis_recetas = mostrar_recetas(mi_categoria)
-        mi_receta = elegir_receta(mis_recetas)
-        # leer recetas
-        # volver inicio
-        pass
-    case 2:
-        mis_categorias = mostrar_categorias(ruta)
-        mi_categoria = elegir_categorias(mis_categorias)
-        # crear receta nueva
-        # volver inicio
-        pass
-    case 3:
-        # crear categoria nueva
-        # volver inicio
-        pass
-    case 4:
-        mis_categorias = mostrar_categorias(ruta)
-        mi_categoria = elegir_categorias(mis_categorias)
-        mis_recetas = mostrar_recetas(mi_categoria)
-        mi_receta = elegir_receta(mis_recetas)
-        # eliminar recetas
-        # volver inicio
-        pass
-    case 5:
-        mis_categorias = mostrar_categorias(ruta)
-        mi_categoria = elegir_categorias(mis_categorias)
-        # eliminar categoria
-        # volver inicio
-        pass
-    case 6:
-        # finalizar programa
-        pass
+while not finalizar:
+    # Mostrar menu inicio
+    menu = inicio()
+
+    match menu:
+        case 1:
+            mis_categorias = mostrar_categorias(ruta)
+            mi_categoria = elegir_categorias(mis_categorias)
+            mis_recetas = mostrar_recetas(mi_categoria)
+            mi_receta = elegir_receta(mis_recetas)
+            leer_receta(mi_receta)
+            volver_inicio()
+            pass
+        case 2:
+            mis_categorias = mostrar_categorias(ruta)
+            mi_categoria = elegir_categorias(mis_categorias)
+            crear_receta(mi_categoria)
+            volver_inicio()
+            pass
+        case 3:
+            crear_categoria(ruta)
+            volver_inicio()
+            pass
+        case 4:
+            mis_categorias = mostrar_categorias(ruta)
+            mi_categoria = elegir_categorias(mis_categorias)
+            mis_recetas = mostrar_recetas(mi_categoria)
+            mi_receta = elegir_receta(mis_recetas)
+            eliminar_receta(mi_receta)
+            volver_inicio()
+            pass
+        case 5:
+            mis_categorias = mostrar_categorias(ruta)
+            mi_categoria = elegir_categorias(mis_categorias)
+            eliminar_categoria(mi_categoria)
+            volver_inicio()
+            pass
+        case 6:
+            finalizar = True
